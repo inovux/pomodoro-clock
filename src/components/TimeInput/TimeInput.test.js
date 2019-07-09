a3cc7cf5b80f5b401db46a06a33599063d79cf14
Stanley Thijssen <stanley.thijssen@energybox.com>
2019-07-09 19:25:09 +0800

    Added test checking whether an input field is rendered

--- src/components/TimeInput/TimeInput.test.js
+++ src/components/TimeInput/TimeInput.test.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 
 import TimeInput from './TimeInput';
-
-afterEach(cleanup);
 
 describe('TimeInput', () => {
   it('renders', () => {
     const { asFragment } = render(<TimeInput />);
 
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('renders an input when number is clicked', () => {
+    const { queryByText, queryByDisplayValue } = render(
+      <TimeInput value={0} />
+    );
+    const span = queryByText('0');
+
+    fireEvent.click(span);
+
+    const inputField = queryByDisplayValue('0');
+
+    expect(inputField).toBeInTheDocument();
   });
 });
